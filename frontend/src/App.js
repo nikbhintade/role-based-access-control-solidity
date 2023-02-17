c18d3bebe46faf551038739a9c826d9c5c0ba3e2
Nikhil <29760727+nikbhintade@users.noreply.github.com>
2023-02-17 21:49:31 +0530

    web app-html strusture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+	return (
+		<div className="App">
+			<div className="no-input">
+				<h2>Role Based Access Control</h2>
+				<button>Connect</button>
+			</div>
+			<hr class="solid"></hr>
+			<div className="row">
+				<h2>Grant Role</h2>
+				<form>
+					<input placeholder="Data Type: Address" />
+					<button type="submit">Grant Role</button>
+				</form>
+			</div>
+			<div className="row">
+				<h2>Revoke Role</h2>
+				<form>
+					<input placeholder="Data Type: Address" />
+					<button type="submit">Revoke Role</button>
+				</form>
+			</div>
+			<div className="no-input">
+				<h2>Renounce Role</h2>
+				<form>
+					<button>Renounce Role</button>
+				</form>
+			</div>
+			<hr class="solid"></hr>
+			<div className="row">
+				<h2>Create Entry</h2>
+				<form>
+					<input placeholder="Data Type: String" />
+					<button type="submit">Create Entry</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default App;
